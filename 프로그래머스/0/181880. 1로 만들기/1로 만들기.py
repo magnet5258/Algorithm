64e95cfd2e1bd9060e9a6e30c60cc7ecd09f3ba5
magnet5258 <152006869+magnet5258@users.noreply.github.com>
2024-05-09 18:00:19 +0900

    [level 0] Title: 1로 만들기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.py
@@ -0,0 +1,13 @@
+def solution(num_list):
+    answer = 0
+    for i in range(len(num_list)):
+        while True:
+            if num_list[i] == 1:
+                break
+            elif num_list[i] % 2 == 0:
+                num_list[i] = int(num_list[i] / 2)
+                answer += 1
+            else:
+                num_list[i]  = int((num_list[i] - 1) / 2)
+                answer += 1
+    return answer
