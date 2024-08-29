e1cfae017bee6ee5d189ef325f8e815f5b13f63b
magnet5258 <152006869+magnet5258@users.noreply.github.com>
2024-08-29 14:48:35 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -1,7 +1,8 @@
-import math
-
 def solution(n, m):
     answer = []
-    answer.append(math.gcd(n, m))
-    answer.append(abs(n * m) // math.gcd(n, m))
+    c, d = max(n, m), min(n, m)
+    while d != 0:
+        c, d = d, c % d
+    answer.append(abs(c))
+    answer.append(n * m // abs(c))
     return answer
