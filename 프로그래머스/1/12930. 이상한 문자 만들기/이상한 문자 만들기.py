4de95c54a34f23dcbb2d7aa694467ed691c10ae1
magnet5258 <152006869+magnet5258@users.noreply.github.com>
2024-09-05 16:31:21 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.06 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -1,14 +1,2 @@
 def solution(s):
-    s_list = list(s)
-    idx = 0
-    for i in range(len(s_list)):
-        if s_list[i].isalpha():
-            if idx % 2 == 0:
-                s_list[i] = s_list[i].upper()
-                idx += 1
-            else:
-                s_list[i] = s_list[i].lower()
-                idx += 1
-        else:
-            idx = 0
-    return ''.join(s_list)
+    return " ".join(map(lambda x: "".join([a.lower() if i % 2 else a.upper() for i, a in enumerate(x)]), s.split(" ")))
