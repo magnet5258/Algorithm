aa972ddc3f9118f5d1f34ab10190e2be3a733ec1
magnet5258 <152006869+magnet5258@users.noreply.github.com>
2024-09-11 16:34:58 +0900

    [level 2] Title: 피보나치 수, Time: 32.79 ms, Memory: 13.7 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
@@ -1,9 +1,6 @@
 def solution(n):
-    if n <= 1:
-        return n
-    else:
-        fib = [0] * (n + 1)
-        fib[0], fib[1] = 0, 1
-        for i in range(2, n + 1):
-            fib[i] = (fib[i - 1] + fib[i - 2]) % 1234567
-        return fib[n]
+    fib = [0] * (n + 1)
+    fib[0], fib[1] = 0, 1
+    for i in range(2, n + 1):
+        fib[i] = (fib[i - 1] + fib[i - 2]) % 1234567
+    return fib[n]
