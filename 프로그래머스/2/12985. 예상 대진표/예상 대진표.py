df07dcc94f3dcac792113b20db3e467e2fdd13c8
magnet5258 <152006869+magnet5258@users.noreply.github.com>
2024-10-14 20:29:33 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
@@ -1,17 +1,6 @@
 def solution(n,a,b):
     answer = 0
     while a != b:
-        if a % 2 == 1 and b % 2 == 1:
-            a = int(a / 2 + 1)
-            b = int(b / 2 + 1)
-        elif a % 2 == 1 and b % 2 == 0:
-            a = int(a / 2 + 1)
-            b = int(b / 2)
-        elif a % 2 == 0 and b % 2 == 1:
-            a = int(a / 2)
-            b = int(b / 2 + 1)
-        else:
-            a = int(a / 2)
-            b = int(b / 2)
+        a, b = (a + 1) // 2, (b + 1) // 2
         answer += 1
     return answer
