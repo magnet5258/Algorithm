7857b44689cb316d46ebb1a32f83acdad3b23950
magnet5258 <152006869+magnet5258@users.noreply.github.com>
2024-11-06 16:29:16 +0900

    [level 1] Title: 2016년, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.py
+++ 프로그래머스/1/12901. 2016년/2016년.py
@@ -1,20 +1,8 @@
 from datetime import datetime
 
 def solution(a, b):
+    days_of_week = ['FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED', 'THU']
     date1 = datetime(2016, 1, 1)
     date2 = datetime(2016, a, b)
     diff = (date2 - date1).days
-    if diff % 7 == 0:
-        return 'FRI'
-    elif diff % 7 == 1:
-        return 'SAT'
-    elif diff % 7 == 2:
-        return 'SUN'
-    elif diff % 7 == 3:
-        return 'MON'
-    elif diff % 7 == 4:
-        return 'TUE'
-    elif diff % 7 == 5:
-        return 'WED'
-    elif diff % 7 == 6:
-        return 'THU'
+    return days_of_week[diff % 7]
--- 프로그래머스/1/12901. 2016년/README.md
+++ 프로그래머스/1/12901. 2016년/README.md
@@ -1,6 +1,6 @@
 # [level 1] 2016년 - 12901 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12901?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12901) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 06일 16:13:27
+2024년 11월 06일 16:29:05
 
 ### 문제 설명
 
