d0b61c5ed31ac1d638ba0544f48c873f0168d051
magnet5258 <152006869+magnet5258@users.noreply.github.com>
2025-01-20 23:53:46 +0900

    [Bronze I] Title: 색종이, Time: 464 ms, Memory: 39196 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/10163. 색종이/색종이.py
+++ 백준/Bronze/10163. 색종이/색종이.py
@@ -3,10 +3,10 @@ arr = [[0] * 1001 for _ in range(1001)]
 for n in range(1, N + 1):
     sj, si, w, h = map(int, input().split())
     for i in range(si, si + h):
-        for j in range(sj, sj + w):
-            arr[i][j] = n
-for n in range(1, N + 1):
-    cnt = 0
-    for lst in arr:
-        cnt += lst.count(n)
-    print(cnt)
+        arr[i][sj:sj + w] = [n] * w
+
+cnts = [0] * (N + 1)
+for lst in arr:
+    for n in lst:
+        cnts[n] += 1
+print(*cnts[1:], sep = '\n')
