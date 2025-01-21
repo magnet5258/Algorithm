ae0c7b5578fc5edb2b3ba16d7ae113122e020ad8
magnet5258 <152006869+magnet5258@users.noreply.github.com>
2025-01-21 23:16:01 +0900

    [Bronze II] Title: 직사각형을 만드는 방법, Time: 88 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/8320. 직사각형을 만드는 방법/직사각형을 만드는 방법.py
+++ 백준/Bronze/8320. 직사각형을 만드는 방법/직사각형을 만드는 방법.py
@@ -3,7 +3,5 @@ ans = 0
 for x in range(1, n + 1):
     for y in range(1, int(x ** 0.5) + 1):
         if x % y == 0:
-            z = x // y
-            if y <= z:
-                ans += 1
+            ans += 1
 print(ans)
