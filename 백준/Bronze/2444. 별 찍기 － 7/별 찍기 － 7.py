e56e08c0d5e0d4d167e6c13da5582c921bca90c5
magnet5258 <152006869+magnet5258@users.noreply.github.com>
2025-02-02 20:29:48 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.py
@@ -0,0 +1,6 @@
+N = int(input())
+for i in range(2 * N - 1):
+    if i < N:
+        print(' ' * (N - 1 - i) + '*' * (2 * i + 1))
+    else:
+        print(' ' * (i - N + 1) + '*' * (2 * (2 * N - 2 - i) + 1))
