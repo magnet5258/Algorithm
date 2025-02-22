7ad5bae71a82699188a7b1a6f54c1f84c119bb68
magnet5258 <152006869+magnet5258@users.noreply.github.com>
2025-02-22 22:40:21 +0900

    [D6] Title: [S/W 문제해결 응용] 10일차 - 작업순서, Time: 97 ms, Memory: 73,856 KB -BaekjoonHub

--- SWEA/D6/1267. ［S／W 문제해결 응용］ 10일차 － 작업순서/［S／W 문제해결 응용］ 10일차 － 작업순서.py
+++ SWEA/D6/1267. ［S／W 문제해결 응용］ 10일차 － 작업순서/［S／W 문제해결 응용］ 10일차 － 작업순서.py
@@ -1,28 +1,23 @@
-from collections import deque
+from collections import defaultdict
+
+def order(v):
+    visited[v] = True
+    for neighbor in graph[v]:
+        if not visited[neighbor]:
+            order(neighbor)
+    result.append(v)
 
 T = 10
 for test_case in range(1, T + 1):
-    V, E = map(int, input().split())
-    lst = list(map(int, input().split()))
-    edges = list(zip(lst[::2], lst[1::2]))
-    nodes = set(range(1, V + 1))
-    graph = {node: [] for node in nodes}
-    in_degree = {node: 0 for node in nodes}
-
-    for i, j in edges:
-        graph[i].append(j)
-        in_degree[j] += 1
-
-    queue = deque([node for node in nodes if in_degree[node] == 0])
-    ans = []
-
-    while queue:
-        node = queue.popleft()
-        ans.append(node)
-
-        for n in graph[node]:
-            in_degree[n] -= 1
-            if in_degree[n] == 0:
-                queue.append(n)
-
-    print(f'#{test_case}', *ans)
+    v_cnt, e_cnt = map(int, input().split())
+    edges = list(map(int, input().split()))
+    graph = defaultdict(list)
+    for i in range(e_cnt):
+        graph[edges[2 * i]].append(edges[2 * i + 1])
+    visited = [False] * (v_cnt + 1)
+    result = []
+    for v in range(1, v_cnt + 1):
+        if not visited[v]:
+            order(v)
+    ans = result[::-1]
+    print(f"#{test_case} {' '.join(map(str, ans))}")
