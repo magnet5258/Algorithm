aa031f513ca0399845c1ed6f3fe878c8532f306c
magnet5258 <152006869+magnet5258@users.noreply.github.com>
2025-03-03 12:14:40 +0900

    [Silver III] Title: 2×n 타일링, Time: 100 ms, Memory: 109544 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -0,0 +1,13 @@
+def cnt_tile(tiles, n):
+    dp = [0] * (n + 1)
+    dp[0] = 1
+    for i in range(1, n + 1):
+        for tile in tiles:
+            if i - tile >= 0:
+                dp[i] += dp[i - tile]
+
+    return dp[n]
+
+n = int(input())
+tiles = [1, 2]
+print(cnt_tile(tiles, n) % 10007)
