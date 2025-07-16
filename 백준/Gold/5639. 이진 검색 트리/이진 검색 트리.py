1ff3b7304c38ebf5a0bf41e33dcde52c033d7473
Yang Donghwan <152006869+magnet5258@users.noreply.github.com>
2025-07-17 00:55:53 +0900

    [Gold IV] Title: 이진 검색 트리, Time: 232 ms, Memory: 136888 KB -BaekjoonHub

--- 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.py
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.py
@@ -1,37 +1,22 @@
 import sys
 sys.setrecursionlimit(15000)
 
-class Node:
-    def __init__(self, value):
-        self.value = value
-        self.left = None
-        self.right = None
-
-def insert(root, value):
-    if root is None:
-        return Node(value)
-    if value < root.value:
-        root.left = insert(root.left, value)
-    else:
-        root.right = insert(root.right, value)
-    return root
-
-def postorder(node):
-    if node is None:
-        return
-    postorder(node.left)
-    postorder(node.right)
-    print(node.value)
-
 preorder = []
 while True:
     try:
         preorder.append(int(sys.stdin.readline()))
-    except:
+    except ValueError:
         break
 
-root = None
-for num in preorder:
-    root = insert(root, num)
+def postorder(start, end):
+    if start > end:
+        return
+    root = preorder[start]
+    idx = start + 1
+    while idx <= end and preorder[idx] < root:
+        idx += 1
+    postorder(start + 1, idx - 1)
+    postorder(idx, end)
+    print(root)
 
-postorder(root)
+postorder(0, len(preorder) - 1)
